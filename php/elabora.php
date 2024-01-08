d562962e2cb7886292c3baf981034357c08d625b
matteomedori <matteo.medori.97@gmail.com>
2024-01-08 16:59:21 +0100

    feat: aggiunto css

--- php/elabora.php
+++ php/elabora.php
@@ -9,15 +9,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP BadWords</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <h2>Testo inserito:</h2>
-    <p><?php echo $_POST['text']; ?></p>
-    <p>Lunghezza del testo: <?php echo strlen($_POST['text']); ?> </p>
+    <div class="container">
+        <h2>Testo inserito:</h2>
+        <p><?php echo $_POST['text']; ?></p>
+        <p>Lunghezza del testo: <?php echo strlen($_POST['text']); ?> </p>
 
-    <h2>Testo con parola nascosta:</h2>
-    <p><?php echo $new_text; ?></p>
-    <p>Lunghezza del testo: <?php echo strlen($new_text); ?> </p>
+        <h2>Testo con parola nascosta:</h2>
+        <p><?php echo $new_text; ?></p>
+        <p>Lunghezza del testo: <?php echo strlen($new_text); ?> </p>
+    </div>
 </body>
 </html>
--- php/index.php
+++ php/index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP BadWords</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Inserire un testo e una parola da nascondere</h1>
     <form action="elabora.php" method="post">
-        <label for="text">Textarea:</label><br>
+        <label for="text" hidden>Textarea:</label><br>
         <textarea name="text" id="text" cols="30" rows="10"></textarea><br>
-        <label for="word">Badword:</label><br>
-        <input type="text" id="word" name="word">
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" id="word" name="word"><br>
         <button type="submit">Vai!</button>
     </form>
 </body>
